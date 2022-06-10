6bb03bb8138afc7222bb9bc155ccf7653fc355ed
Liam Sho <conan_creeper@outlook.com>
2022-06-10 22:14:41 +0800

    chore: remove macos button

--- apps/web/src/components/home/HomeActions/HomeActions.tsx
+++ apps/web/src/components/home/HomeActions/HomeActions.tsx
@@ -2,7 +2,6 @@ import { Icon } from '@iconify/react';
 import { FC } from 'react';
 import { GlowButton } from '../../foundations/GlowButton/GlowButton';
 import mdiWindows from '@iconify/icons-mdi/windows';
-import mdiApple from '@iconify/icons-mdi/apple';
 import mdiGitHub from '@iconify/icons-mdi/github';
 
 const platforms = [
@@ -11,19 +10,13 @@ const platforms = [
     title: 'Windows',
     url: 'https://github.com/MaaAssistantArknights/MaaAssistantArknights/',
     version: 'v3.9.9 (2022.6.5 18:32)',
-  },
-  {
-    icon: mdiApple,
-    title: 'macOS',
-    url: 'https://github.com/MaaAssistantArknights/MaaAssistantArknights/',
-    version: 'v3.9.9 (2022.6.5 18:32)',
-  },
+  }
 ];
 
 export const HomeActions: FC = () => {
   return (
     <div className="absolute bottom-0 left-0 right-0 mb-24 md:mb-[7vh] flex flex-col mx-8">
-      <div className="hidden md:flex flex-col md:flex-row items-center justify-center font-light gap-2 md:gap-6">
+      <div className="flex-col items-center justify-center hidden gap-2 font-light md:flex md:flex-row md:gap-6">
         {platforms.map((platform) => (
           <GlowButton>
             <div className="flex flex-col items-start">
@@ -39,22 +32,22 @@ export const HomeActions: FC = () => {
         ))}
       </div>
 
-      <div className="flex md:hidden flex-row items-center justify-center">
+      <div className="flex flex-row items-center justify-center md:hidden">
         <GlowButton>
-          <div className="flex items-center text-sm -ml-1">
+          <div className="flex items-center -ml-1 text-sm">
             <Icon icon={mdiGitHub} fontSize="20px" />
             <span className="ml-2">GitHub</span>
           </div>
         </GlowButton>
       </div>
 
-      <div className="mt-6 md:mt-8 text-white/70 text-xs leading-5 text-center">
+      <div className="mt-6 text-xs leading-5 text-center md:mt-8 text-white/70">
         MAA 以 AGPL-3.0 协议开源；使用即表示您同意并知悉「用户协议」的相关内容。
       </div>
 
-      <div className="hidden md:flex flex-row items-center justify-center mt-4">
+      <div className="flex-row items-center justify-center hidden mt-4 md:flex">
         <GlowButton translucent href="https://github.com/MaaAssistantArknights">
-          <div className="flex items-center text-sm -ml-1">
+          <div className="flex items-center -ml-1 text-sm">
             <Icon icon={mdiGitHub} fontSize="20px" />
             <span className="ml-2">GitHub</span>
           </div>
