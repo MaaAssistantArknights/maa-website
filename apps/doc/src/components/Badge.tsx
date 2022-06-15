cdd61b8e2d5cb5833fdcc72b1d2c1e3948675839
Liam Sho <conan_creeper@outlook.com>
2022-06-15 20:04:25 +0800

    style: fix code style

--- apps/doc/src/components/Badge.tsx
+++ apps/doc/src/components/Badge.tsx
@@ -1,11 +1,7 @@
-import React, { memo } from 'react'
-import { FCC } from '../types'
-import style from './Badge.module.css'
+import React, { memo } from 'react';
+import { FCC } from '../types';
+import style from './Badge.module.css';
 
-export const Badge: FCC<{ src: string, alt: string }> = memo(
-  ({ src, alt }) => {
-    return (
-      <img className={style.space} src={src} alt={alt} />
-    )
-  }
-)
+export const Badge: FCC<{ src: string; alt: string }> = memo(({ src, alt }) => {
+  return <img className={style.space} src={src} alt={alt} />;
+});
