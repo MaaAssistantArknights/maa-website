2078d4ca07a6e3dc9255755b16ce29b32981d064
GalvinGao <me@galvingao.com>
2022-06-24 13:46:01 +0800

    style(lint): fix lint errors

--- apps/doc/src/components/Badge.tsx
+++ apps/doc/src/components/Badge.tsx
@@ -1,6 +1,6 @@
-import React, { memo } from 'react';
 import { FCC } from '../types';
 import style from './Badge.module.css';
+import React, { memo } from 'react';
 
 export const Badge: FCC<{ src: string; alt: string }> = memo(({ src, alt }) => {
   return <img className={style.space} src={src} alt={alt} />;
