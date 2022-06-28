4320fac0d785cf08f4311c547616cad91c627707
GalvinGao <me@galvingao.com>
2022-06-28 19:28:53 +0800

    feat: add sentry support

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,3 +1,6 @@
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { SWRConfig } from 'swr'
@@ -6,6 +9,12 @@ import App from './App'
 import { fetch } from './utils/fetch'
 
 import './index.css'
+
+Sentry.init({
+  dsn: 'https://6e0c079fe21c424189cc0f89ba7bb029@o1299554.ingest.sentry.io/6536125',
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 0.01,
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
