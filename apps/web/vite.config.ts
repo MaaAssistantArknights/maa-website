9c67454c86039c9594446008331ca90ee0d03b72
GalvinGao <me@galvingao.com>
2022-07-21 15:48:37 +0800

    build: disable rollup-plugin-analyzer by default

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), analyze()],
+  plugins: [react(), ...[process.env.VITE_ENABLE_ANALYZER !== '' && analyze()]],
 })
