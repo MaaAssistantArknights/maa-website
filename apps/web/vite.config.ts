0f8db120605093863149724c590ef246f6231b84
GalvinGao <me@galvingao.com>
2022-07-21 16:23:20 +0800

    build: fix analyzer

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,5 +5,8 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ...[process.env.VITE_ENABLE_ANALYZER !== '' && analyze()]],
+  plugins: [
+    react(),
+    ...[process.env.VITE_ENABLE_ANALYZER === 'true' && analyze()],
+  ],
 })
