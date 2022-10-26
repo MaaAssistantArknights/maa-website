293728c42dda9e887aa80321fcf9a2be75341833
_ChingC <chingc.ccmail@gmail.com>
2022-10-26 19:31:31 +0800

    refactor: move doc to vuepress

---
+++ apps/doc/apps/doc/.vuepress/.temp/internal/pagesComponents.js
@@ -0,0 +1,6 @@
+import { defineAsyncComponent } from 'vue'
+
+export const pagesComponents = {
+  // path: /404.html
+  "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"/Users/_chingc/Documents/repository/MaaAssistantArknights/maa-website/apps/doc/apps/doc/.vuepress/.temp/pages/404.html.vue")),
+}
